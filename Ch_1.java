c7608d000afe35afa2ade84cd251762c69c0a3da
Nitinpal269 <76909634+Nitinpal269@users.noreply.github.com>
2021-03-26 22:18:02 +0530

    Add files via upload

---
+++ Ch_1.java
@@ -0,0 +1,12 @@
+package com.ducat;
+
+//primitive data types
+public class Ch_1 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        int sum=a+b+c;
+        System.out.println("The sum of three number is: "+sum);
+    }
+}
---
+++ Ch_1_Literals.java
@@ -0,0 +1,25 @@
+package com.ducat;
+
+public class Ch_1_Literals {
+    public static void main(String[] args) {
+        int a=10;
+        short b=20;
+        char agg='b';
+        long c=23445666666666666l;
+        float d=12.23f;
+        double e=12.455555;
+        byte f=122;
+        boolean g=true;
+        String str="nitin";
+
+        System.out.println("Integer number: "+a);
+        System.out.println("Short number: "+b);
+        System.out.println("Character char: "+agg);
+        System.out.println("Long number: "+c);
+        System.out.println("Float number: "+d);
+        System.out.println("Double number: "+e);
+        System.out.println("Byte number: "+f);
+        System.out.println("Boolean : "+g);
+        System.out.println("String: "+str);
+    }
+}
