3bb862820ad0e7f8e26300368c02a22750cacf43
Nitinpal269 <76909634+Nitinpal269@users.noreply.github.com>
2021-03-27 22:56:04 +0530

    Add files via upload

---
+++ Ch_2_Operator.java
@@ -0,0 +1,18 @@
+package com.ducat;
+
+public class Ch_2_Operator {
+    public static void main(String[] args) {
+        int a=4;
+        int b=9;
+        b=b+4;//arithmatic operator
+        System.out.println(b);
+        int c=4;// assignment oprator
+        int d = 6 % c;// modulo operator
+        System.out.println(d);
+        System.out.println(6==8);// comparison operator
+        System.out.println(64>5 && 56>5);// Logical operator
+        System.out.println(64>5 || 64>98);// logical operator
+        System.out.println(2&3);// bitwise operator
+
+    }
+}
